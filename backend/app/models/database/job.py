3ea02f15deb8dfa3ec057c1763f540228833516d
sabry558 <ahmedsabry2002218@gmail.com>
2026-06-21 17:01:55 +0300

    create db schemas
    created job schema
    created refresh_token schema
    create user schema

---
+++ backend/app/models/database/job.py
@@ -0,0 +1,38 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, Enum, ForeignKey, String
+from sqlalchemy.orm import relationship
+from db.base import Base
+
+
+
+class JobType(str, enum.Enum):
+    IMAGE = "image"
+    VIDEO = "video"
+    CLASSIFIER= "classifier"
+
+
+class JobStatus(str, enum.Enum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+class Job(Base):
+    __tablename__ = "job"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    user_id = Column(BigInteger, ForeignKey("users.id"), nullable=False, index=True)
+    type = Column(Enum(JobType, name="job_type"), nullable=False)
+    status = Column(Enum(JobStatus, name="job_status"), nullable=False, default=JobStatus.PENDING)
+    description = Column(String, nullable=True) 
+    source_path = Column(String, nullable=False)
+    target_path = Column(String, nullable=True)
+    is_seen = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False
+    )
+
+    user = relationship("User", back_populates="jobs")
---
+++ backend/app/models/database/user.py
@@ -0,0 +1,22 @@
+from datetime import date, datetime
+
+from sqlalchemy import BigInteger, Boolean, Column, Date, DateTime,String
+from sqlalchemy.orm import relationship
+
+from db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    last_login = Column(DateTime, nullable=True)
+    created_at = Column(Date, default=date.today, nullable=False)
+
+    refresh_tokens = relationship("RefreshToken", back_populates="user")
+    jobs = relationship("Job", back_populates="user")
+
