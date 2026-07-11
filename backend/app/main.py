506ca6214d0ac2d12a4538db9a9e403b4fe68a98
sabry558 <ahmedsabry2002218@gmail.com>
2026-07-11 20:29:21 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.api.authentication import authentication_router
+from app.api.classifier import classifier_router
 from app.db.init_db import init_db
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    print("Starting application...")
     await init_db()
 
 app.include_router(authentication_router)
+app.include_router(classifier_router)
