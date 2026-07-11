877aa00bd1006607cfbbe8831bc74ffb7b3a13b0
sabry558 <ahmedsabry2002218@gmail.com>
2026-07-11 20:27:02 +0300

    update job.py
    add new column to the table
    delete description column

--- backend/app/models/database/job.py
+++ backend/app/models/database/job.py
@@ -6,16 +6,17 @@ from sqlalchemy.orm import relationship
 from app.db.base import Base
 
 
-
 class JobType(str, enum.Enum):
     IMAGE = "image"
     VIDEO = "video"
-    CLASSIFIER= "classifier"
+    VIDEO_CLASSIFICATION = "video_classifier"
+    IMAGE_CLASSIFICATION = "image_classifier"
 
 
 class JobStatus(str, enum.Enum):
     PENDING = "pending"
     COMPLETED = "completed"
+    RUNNING = "running"
     FAILED = "failed"
 
 
@@ -26,13 +27,20 @@ class Job(Base):
     user_id = Column(BigInteger, ForeignKey("users.id"), nullable=False, index=True)
     type = Column(Enum(JobType, name="job_type"), nullable=False)
     status = Column(Enum(JobStatus, name="job_status"), nullable=False, default=JobStatus.PENDING)
-    description = Column(String, nullable=True) 
+    original_name = Column(String, nullable=False)
     source_path = Column(String, nullable=False)
     target_path = Column(String, nullable=True)
     is_seen = Column(Boolean, default=False, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
     updated_at = Column(
-        DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc), nullable=False
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
     )
 
     user = relationship("User", back_populates="jobs")
